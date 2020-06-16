64e5b1b42e838dcc2391a247038eba0163c75343
AKADRA-SAKURA <akadra.sakura7@gmail.com>
2020-06-16 22:33:41 +0900

    1stcommit

---
+++ app/controllers/hello_controller.rb
@@ -0,0 +1,24 @@
+class HelloController < ApplicationController
+    
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def search
+    end
+
+    def mypage
+    end
+
+    def link
+    end
+
+end
