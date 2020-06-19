da0f9acd4474a36db63a798c75201b496140660f
AKADRA-SAKURA <akadra.sakura7@gmail.com>
2020-06-19 17:24:23 +0900

    検索ページ

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,12 @@
 class SearchController < ApplicationController
 
     def search
+        if params[:search] != nil && params[:search] != ''
+            #部分検索かつ複数検索
+            @tweets = Tweet.where("body LIKE ? ", "%" + params[:search] + "%").or(Tweet.where("tweet.user.name LIKE ? ", "%" + params[:search] + "%"))
+        else
+            @tweets = Tweet.all
+        end
     end
 
     def result
