a49d0da4faca60060d32a4065b34d409a26c2ba6
AKADRA-SAKURA <akadra.sakura7@gmail.com>
2020-06-19 22:44:22 +0900

    投稿者表示まで実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_19_090652) do
+ActiveRecord::Schema.define(version: 2020_06_19_132938) do
 
   create_table "genres", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_06_19_090652) do
     t.text "published"
     t.text "record"
     t.text "online"
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
