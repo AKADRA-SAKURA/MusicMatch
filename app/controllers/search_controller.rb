2f985e97969f6edac162d903d344d63c38b11004
AKADRA-SAKURA <akadra.sakura7@gmail.com>
2020-07-16 02:11:47 +0900

    0715

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,6 @@ class SearchController < ApplicationController
 
     def search
         if params[:search] != nil && params[:search] != ''
-            #部分検索かつ複数検索
             @tweets = Tweet.where("title LIKE ? ", "%" + params[:search] + "%").or(Tweet.where("artist LIKE ? ", "%" + params[:search] + "%")).or(Tweet.where("used LIKE ? ", "%" + params[:search] + "%")).or(Tweet.where("composer LIKE ? ", "%" + params[:search] + "%")).or(Tweet.where("writer LIKE ? ", "%" + params[:search] + "%")).or(Tweet.where("record LIKE ? ", "%" + params[:search] + "%")).or(Tweet.where("published LIKE ? ", "%" + params[:search] + "%")).or(Tweet.where("online LIKE ? ", "%" + params[:search] + "%"))
           else
             @tweets = Tweet.all.order(created_at: :desc)
@@ -14,9 +13,9 @@ class SearchController < ApplicationController
       if params[:tag_ids] != nil && params[:tag_ids] != '' 
         selected_tag_ids = selected_tags_params 
         filtered_tweet_ids = filter(selected_tag_ids) 
-        @tag = Tweet.where(id: filtered_tweet_ids) 
+        @tag = Tweet.where(id: filtered_tweet_ids).page(params[:page]).per(20).order(created_at: :desc).includes(:user) 
       else
-        @tag = Tweet.all
+        @tag = Tweet.all.all.page(params[:page]).per(20).order(created_at: :desc).includes(:user)
       end
     end
   
@@ -30,15 +29,15 @@ class SearchController < ApplicationController
         params.require(:tag_ids)
       end
 
-      def filter(selected_tag_ids) # Tag id array checked with form.
-        selected_tweet_ids = TagRelation.where(tag_id: selected_tag_ids).pluck(:tweet_id).uniq.sort # Memo id array associated to checked tag.
+      def filter(selected_tag_ids)
+        selected_tweet_ids = TagRelation.where(tag_id: selected_tag_ids).pluck(:tweet_id).uniq.sort 
         return Tweet.all unless selected_tag_ids 
-        tweet_filtered = [] # Memo id array associated to all tags checked with form.
+        tweet_filtered = [] 
         selected_tweet_ids.each do |id|
           loop_count = 0
           while selected_tag_ids.length > loop_count do
             break unless Tweet.includes(:tags).find(id).tags.pluck(:id).include?(selected_tag_ids[loop_count].to_i)
-            loop_count += 1; # Get memo id that associated tag id is equall to all tag id selected.
+            loop_count += 1; 
           end
           tweet_filtered << id if loop_count == selected_tag_ids.length
         end
