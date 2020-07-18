ea96c7acb0681b283976cf33d5385782763a9a71
AKADRA-SAKURA <akadra.sakura7@gmail.com>
2020-07-18 10:32:29 +0900

    0718

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root 'hello#index'
+
   get 'hello/index' => 'hello#index'
-  get 'hello/link' => 'hello#link'
 
   get 'tweets' => 'tweets#index'
   get 'tweets/new' => 'tweets#new'
