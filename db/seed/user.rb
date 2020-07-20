aef09cd82e4d255b2cdbe577c8754b1d556eb845
AKADRA-SAKURA <akadra.sakura7@gmail.com>
2020-07-20 20:23:09 +0900

    usershokyo

--- db/seed/user.rb
+++
@@ -1,6 +0,0 @@
-User.create!([
-  {email: "ao@gmail.com", encrypted_password: "xaxaxaxa", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_name: "AOだぞ", icon_url: nil},
-  {email: "bb@io.com", encrypted_password: "ABC", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_name: nil, icon_url: nil},
-  {email: "mikametara5@gmail.com", encrypted_password: "111111", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_name: "ミカメタラ", icon_url: nil},
-  {email: "akadra.sakura7@gmail.com", encrypted_password: "2vDxL2S8aMrPmLD", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: nil, user_name: "AKADRA", icon_url: "1534048220921.jpg"}
-])
