48d03e64d584e1d2ef0089c628a6633c709c6e75
AKADRA-SAKURA <akadra.sakura7@gmail.com>
2020-07-20 22:09:16 +0900

    yarinaoshi

--- db/seed/comment.rb
+++ db/seed/comment.rb
@@ -7,6 +7,6 @@ Comment.create!([
   {comment: "うーー", user_id: 1, tweet_id: 7},
   {comment: "ああああ", user_id: 1, tweet_id: 7},
   {comment: "大切な人をなくした時というテーマですよね、名曲です", user_id: 1, tweet_id: 15},
-  {comment: "", user_id: 4, tweet_id: 31},
-  {comment: "カッコいいですこれ\r\n", user_id: 4, tweet_id: 17}
+  {comment: "", user_id: 1, tweet_id: 31},
+  {comment: "カッコいいですこれ\r\n", user_id: 1, tweet_id: 17}
 ])
--- db/seed/like.rb
+++ db/seed/like.rb
@@ -2,7 +2,7 @@ Like.create!([
   {tweet_id: 7, user_id: 1},
   {tweet_id: 9, user_id: 3},
   {tweet_id: 17, user_id: 3},
-  {tweet_id: 17, user_id: 4},
-  {tweet_id: 18, user_id: 4},
-  {tweet_id: 14, user_id: 4}
+  {tweet_id: 17, user_id: 1},
+  {tweet_id: 18, user_id: 1},
+  {tweet_id: 14, user_id: 1}
 ])
