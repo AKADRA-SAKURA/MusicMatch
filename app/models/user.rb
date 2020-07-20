90e20b4171e4aaacaa90c9014c617d00a3ae8d0d
AKADRA-SAKURA <akadra.sakura7@gmail.com>
2020-07-20 21:18:04 +0900

    useroptional

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :tweets, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  has_many :tweets, dependent: :destroy, optional: true
+  has_many :likes, dependent: :destroy, optional: true
   has_many :liked_tweets, through: :likes, source: :tweet
-  has_many :comments, dependent: :destroy
+  has_many :comments, dependent: :destroy, optional: true
   mount_uploader :icon_url, ImageUploader
-  has_many :tag
+  has_many :tag, optional: true
 
   def already_liked?(t)
     self.likes.exists?(tweet_id: t.id)
